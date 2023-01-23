1dd4129cdfffd542a4a4525a32d2965be35f2a6e
D <231689@gmail.com>
2023-01-23 12:05:49 +1100

    Start pseudo-coding instructions from Odin project brief

---
+++ index.js
@@ -0,0 +1,14 @@
+// ORIGINAL SET OF INSTRUCTIONS FROM ODIN (https://www.theodinproject.com/lessons/foundations-rock-paper-scissors):
+
+// You will play against the computer
+
+// 1. Function 1: getComputerChoice() - this should randomly return either "rock", "paper" or "scissors". This function will the be computer's play.
+
+// 2. Function 2: playRound() - a function that plays a single round of the game. The function should take 2 parameters - "playerSelection" and "computerSelection"; then return a string that declares the winner of the round: "You lose! Paper beats Rock."
+//         Note: make this function's playerSelection parameter case-insensitive (so users can input rock, rOck, ROck or any other variation).
+//         Note: you want to return the results of this function call, NOT console.log() them. You are going to use what you return later on, so use console.log to see the results.
+
+// 3. Function 3: game(). Call playRound() inside of game() to play a 5 round game that keeps score and reports a winner or loser at the end.
+
+// Other tips:
+// - use prompt() to get input from the user.
